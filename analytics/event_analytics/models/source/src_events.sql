3f7ea41a1fba511c6eb292361ac85261c39506a1
Gaurav Ravishankar Rokade <gauravravirokade@gmail.com>
2025-08-17 13:32:37 -0400

    Parse event context.

--- analytics/event_analytics/models/source/src_events.sql
+++ analytics/event_analytics/models/source/src_events.sql
@@ -1,4 +1,38 @@
+/*
+ Purpose - raw event data from Postgres; parses event_context to get event_id, publisher_id, domain, url, device_id, user_agent, and page_view_id.
+Grain: one row per event.
+ */
+
+WITH
+    base AS (SELECT
+                 *
+                 -- Cast JSON columns to JSONB
+               , event_context::jsonb AS context
+               , event_data::jsonb    AS event_data_json
+             FROM
+                 {{ source('raw', 'raw_events') }}
+-- raw.raw_events
+    )
+
+  , final AS (SELECT
+                  event_created_at
+                , event_name
+                  -- Parsed context fields
+                , context ->> 'eid'          AS event_id
+                , context ->> 'publisher_id' AS publisher_id
+                , context ->> 'domain'       AS domain
+                , context ->> 'url'          AS url
+                , context ->> 'did'          AS device_id
+                , context ->> 'ua'           AS user_agent
+                , context ->> 'pvid'         AS page_view_id
+                , event_data_json
+                , context
+                , source_file_name
+              FROM
+                  base)
+
 SELECT
     *
 FROM
-    {{ source('raw', 'raw_events') }}
+    final
+
